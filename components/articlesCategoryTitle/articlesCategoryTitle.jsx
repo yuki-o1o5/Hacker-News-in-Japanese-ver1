837b428b0244f84a934ec17c02d32e1161c7f588
Yuki Kasugai <108368307+yuki-o1o5@users.noreply.github.com>
2022-12-01 15:04:59 -0800

    Delete components/articlesCategoryTitle directory

--- components/articlesCategoryTitle/articlesCategoryTitle.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./ArticlesCategoryTitle.module.css";
-
-const ArticlesCategoryTitle = (props) => {
-  return <p className={styles.categoryTitle}>{props.categoryTitle}</p>;
-};
-
-export default ArticlesCategoryTitle;
