592988cac53dd77de8e8af65a9a34818a3e7eaa7
Yuki Kasugai <108368307+yuki-o1o5@users.noreply.github.com>
2022-12-01 15:04:46 -0800

    Delete components/articleTitle directory

--- components/articleTitle/articleTitle.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./ArticleTitle.module.css";
-
-const ArticleTitle = ({ atitle }) => {
-  return <h2 className={styles.article_title}>{atitle}</h2>;
-};
-
-export default ArticleTitle;
