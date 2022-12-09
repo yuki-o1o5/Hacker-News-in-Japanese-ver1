8bdb139a6b117c702db1fe67db33c8bfef388b7a
yuki-o1o5 <jkksst1712@gmail.com>
2022-12-09 04:40:06 -0800

    chore_remove_superjson

--- next.config.js
+++ next.config.js
@@ -1,27 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const { withSuperjson } = require("next-superjson");
-
-module.exports = withSuperjson()({});
-
-// const nextConfig = {
-//   reactStrictMode: false,
-//   swcMinify: true,
-// };
-
-// module.exports = nextConfig;
-
-// module.exports = {
-//   reactStrictMode: false,
-//   swcMinify: true,
-//   webpack: (config, { isServer }) => {
-//     if (!isServer) {
-//       // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-//       config.resolve.fallback = {
-//         fs: false,
-//       };
-//     }
-
-//     return config;
-//   },
-// };
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+};
