1aadd2f6d19a3c40f69a59290de62d769a80f77b
Yuki Kasugai <108368307+yuki-o1o5@users.noreply.github.com>
2022-12-09 18:35:52 -0800

    Delete components/articleNumber directory

--- components/articleNumber/articleNumber.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./ArticleNumber.module.css";
-
-const ArticleNumber = ({ articleNumber }) => {
-  return <i className={styles.number}>{articleNumber}</i>;
-};
-
-export default ArticleNumber;
