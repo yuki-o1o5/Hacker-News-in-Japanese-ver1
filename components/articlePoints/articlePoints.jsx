ef94bc91fde9cb091033ce2727c1b95936eb3b17
Yuki Kasugai <108368307+yuki-o1o5@users.noreply.github.com>
2022-12-13 03:49:24 -0800

    Delete components/articlePoints directory

--- components/articlePoints/articlePoints.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./ArticlePoints.module.css";
-
-const ArticlePoints = ({ points }) => {
-  return <p className={styles.points}>{points}</p>;
-};
-
-export default ArticlePoints;
-
-//
