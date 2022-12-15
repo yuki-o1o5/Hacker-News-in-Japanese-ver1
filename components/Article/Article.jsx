18bf92199e58397a5b55223356421d73dda5a402
yuki-o1o5 <jkksst1712@gmail.com>
2022-12-15 14:02:30 -0800

    fix hydration error

--- components/Article/Article.jsx
+++ components/Article/Article.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import ArticleTitle from "../ArticleTitle/ArticleTitle.jsx";
 import ArticleAuthor from "../ArticleAuthor/ArticleAuthor.jsx";
 import ArticleTime from "../ArticleTime/ArticleTime.jsx";
@@ -5,34 +6,41 @@ import ArticlePoints from "../ArticlePoints/ArticlePoints.jsx";
 import ArticleNumber from "../ArticleNumber/ArticleNumber.jsx";
 import styles from "./Article.module.css";
 import Link from "next/link.js";
-import React from "react";
 
 const Article = ({ articleTitle, author, time, points, id, index }) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   // console.log(id);
   return (
-    <Link href={"main/" + id}>
-      <React.Fragment>
-        <div className={styles.article_primary_container}>
-          <div className={styles.article_number_container}>
-            <ArticleNumber articleNumber={index} />
-          </div>
-          <div className={styles.article_secondry_container}>
-            <ArticleTitle articleTitle={articleTitle} />
-            <div className={styles.article_tertiary_container}>
-              <div className={styles.tertiary_container_children}>
-                <ArticleAuthor author={author} />
-              </div>
-              <div className={styles.tertiary_container_children}>
-                <ArticleTime time={time} />
-              </div>
-              <div className={styles.tertiary_container_children}>
-                <ArticlePoints points={points} />
+    <>
+      {isClient ? (
+        <Link href={"main/" + id}>
+          <div className={styles.article_primary_container}>
+            <div className={styles.article_number_container}>
+              <ArticleNumber articleNumber={index} />
+            </div>
+            <div className={styles.article_secondry_container}>
+              <ArticleTitle articleTitle={articleTitle} />
+              <div className={styles.article_tertiary_container}>
+                <div className={styles.tertiary_container_children}>
+                  <ArticleAuthor author={author} />
+                </div>
+                <div className={styles.tertiary_container_children}>
+                  <ArticleTime time={time} />
+                </div>
+                <div className={styles.tertiary_container_children}>
+                  <ArticlePoints points={points} />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </React.Fragment>
-    </Link>
+        </Link>
+      ) : null}
+    </>
   );
 };
 
