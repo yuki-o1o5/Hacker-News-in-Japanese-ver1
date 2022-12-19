9cd41e3f4fb303d68f2c6dda46bd1b98a23030c2
yuki-o1o5 <jkksst1712@gmail.com>
2022-12-19 11:32:34 -0800

    fix Can't resolve 'fs’

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
+// const nextConfig = {
+//   reactStrictMode: false,
+//   swcMinify: true,
+// };
+
+module.exports = {
   reactStrictMode: false,
   swcMinify: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
 };
